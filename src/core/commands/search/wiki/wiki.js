60527594ae863419ea20a2e59efa3fdfe849135f
maruf99 <25273018+maruf99@users.noreply.github.com>
2020-12-14 22:49:10 -0500

    fix wiki filter

--- src/core/commands/search/wiki/wiki.js
+++ src/core/commands/search/wiki/wiki.js
@@ -74,7 +74,7 @@ module.exports = class extends Command {
 
     async getBadWords() {
         const url = "https://raw.githubusercontent.com/RobertJGabriel/Google-profanity-words/master/list.txt";
-        const body = await fetch(url, null, "text");
+        const body = await fetch(url, null, "text").split("\n");
         return body;
     }
 };
