f2a423898dfc05ad28feafd3860fbd115cbf4387
maruf99 <25273018+maruf99@users.noreply.github.com>
2020-12-21 10:31:34 -0500

    update Paginator

--- src/structures/Paginator.js
+++ src/structures/Paginator.js
@@ -1,14 +1,27 @@
 const { emojis } = require('../util/constants');
 
 module.exports = class Paginator {
-    constructor(embeds, time) {
+    constructor({ embeds = [], time = 30000 }) {
         this.embeds = embeds;
         this.time = time;
         this.page = 0;
     }
 
+    addEmbed(embed) {
+        if (!embed || typeof embed !== 'object') throw new Error('No Embed provided.');
+        this.embeds.push(embed);
+        return this;
+    }
+
+    setTime(time) {
+        if (typeof time === 'string') time = parseInt(time);
+        if (isNaN(time)) throw new Error('Invalid time.');
+        this.time = time;
+        return this;
+    }
+
     async send(message) {
-        const msg = await message.embed(this.embeds[0]);
+        const msg = await message.util.send(this.embeds[0]);
         for (const emoji of emojis) await msg.react(emoji);
 
         const filter = ({ emoji }, u) => message.author.id === u.id && emojis.includes(emoji.id);
@@ -17,19 +30,19 @@ module.exports = class Paginator {
         collector.on('collect', ({ emoji }, u) => {            
             switch (emojis.indexOf(emoji.id)) {
                 case 0: // First Page
-                    if (this.page > 0) msg.editEmbed(this.embeds[0]);
+                    if (this.page > 0) msg.edit(this.embeds[0]);
                     this.page = 0;
                     break;
                 case 1: // Previous Page
-                    if (this.embeds[this.page - 1]) msg.editEmbed(this.embeds[this.page - 1]);
+                    if (this.embeds[this.page - 1]) msg.edit(this.embeds[this.page - 1]);
                     this.page--;
                     break;
                 case 2: // Next Page
-                    if (this.embeds[this.page + 1]) msg.editEmbed(this.embeds[this.page + 1]);
+                    if (this.embeds[this.page + 1]) msg.edit(this.embeds[this.page + 1]);
                     this.page++;
                     break;
                 case 3: // Last Page
-                    if (this.page < (this.embeds.length - 1)) msg.editEmbed(this.embeds[this.embeds.length - 1]);
+                    if (this.page < (this.embeds.length - 1)) msg.edit(this.embeds[this.embeds.length - 1]);
                     this.page = this.embeds.length - 1;
                     break;
                 case 4: // Stop
