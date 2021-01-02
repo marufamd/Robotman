05c00045c51894e729d26be1170b32cf244551a4
maruf99 <25273018+maruf99@users.noreply.github.com>
2021-01-01 20:45:55 -0500

    replace chalk with colorette

--- src/util/logger.js
+++ src/util/logger.js
@@ -1,5 +1,5 @@
 const moment = require('moment-timezone');
-const { gray, bold } = require('chalk');
+const colorette = require('colorette');
 const { WebhookClient } = require('discord.js');
 
 const development = process.env.NODE_ENV === 'development';
@@ -10,28 +10,30 @@ const { logTypes, botColors, formats } = require('./constants');
 const logger = new WebhookClient(id, token);
 
 module.exports = class Logger {
-    static log(text, type = 'log', { logToWebhook = true, ping = false } = {}) {
+    static log(text, type = 'log', { logToWebhook = true, ping = false } = {}, extra) {
         if (Array.isArray(text)) text = text.join('\n');
         if (!logTypes[type.toLowerCase()]) type = 'log';
 
         Logger.write(text.replaceAll(/(```(\w+)?|`|\*|__|~~)/g, ''), type);
-        if (logToWebhook && id && token) Logger.webhook(text, type, ping);
+        if (logToWebhook && id && token) Logger.webhook(text, type, ping, extra);
     }
 
     static write(text, type = 'log') {
         return console[type](
-            gray(`[${moment().format(formats.log)}] `),
-            bold[logTypes[type].name](text)
+            colorette.gray(`[${moment().format(formats.log)}] `),
+            colorette.bold(colorette[logTypes[type].name](text))
         );
     }
 
-    static webhook(text, type = 'log', ping = false) {
+    static webhook(text, type = 'log', ping = false, extra) {
         const mode = logTypes[type];
         const embed = {
             title: mode.title + (development ? ' (Development)' : ''),
             color: botColors[mode.name],
             footer: { text: moment().tz('America/Toronto').format(formats.log) }
         };
+
+        if (extra && typeof extra === 'object') Object.assign(embed, extra);
 
         try {
             if (text.length < 2040) {
