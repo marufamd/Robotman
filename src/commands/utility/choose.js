f7ef0a294c718ce1d293e596b09a222adca3cb9f
maruf99 <25273018+maruf99@users.noreply.github.com>
2021-01-08 11:06:41 -0500

    update choose

--- src/commands/utility/choose.js
+++ src/commands/utility/choose.js
@@ -2,7 +2,7 @@ const { Command } = require('discord-akairo');
 const request = require('node-superfetch');
 const { randomResponse } = require('../../util');
 
-const URL_REGEX = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gi;
+const URL_REGEX = /^(https?:\/\/(www\.)?)?[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gi;
 
 module.exports = class extends Command {
     constructor() {
@@ -31,6 +31,7 @@ module.exports = class extends Command {
             const url = options.split(' ')[0];
             const res = await request.get(url).then(res => res.text).catch(() => null);
             if (res) options = res.split('\n');
+            else options = options.split('|').map(r => r.trim());
         } else {
             options = options.split('|').map(r => r.trim());
         }
