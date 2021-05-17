96a3b768379fab2efc88fe81a6f03970733b05d4
maruf99 <25273018+maruf99@users.noreply.github.com>
2021-05-16 20:16:22 -0400

    feat: update taste test formatting

--- src/listeners/client/message.ts
+++ src/listeners/client/message.ts
@@ -1,6 +1,7 @@
 import { Listener } from 'discord-akairo';
 import { Message, Permissions } from 'discord.js';
-import { scrapeRedditWiki } from '../../util';
+import { scrapeRedditWiki, split } from '../../util';
+import { colors } from '../../util/constants';
 
 export default class extends Listener {
     public constructor() {
@@ -14,14 +15,23 @@ export default class extends Listener {
         if (/^taste test$/i.test(message.content)) {
             const body = await scrapeRedditWiki(`recsbot/tastetest`, 'DCcomics');
             if (!body || body.kind !== 'wikipage') return;
-            return message.util.send(
-                body.data.content_md
-                    .split('\r\n\r\n')
-                    .join('\n')
-                    .replace(/\[(.+?)\]\((https?:\/\/[a-zA-Z0-9/.(]+?)\)/g, '$1')
-                    .replace(/##\*\*(Mods|Boosters)\*\*/g, '\n**$1**')
-                    .replaceAll('amp:', '')
-            );
+            let [desc, mods, boosters] = this.formatData(body.data.content_md).split('\n\n');
+
+            mods = mods
+                .split('\n')
+                .splice(1)
+                .join('\n');
+
+            const [first, second] = split(boosters.split('\n').splice(1), 15);
+
+            const embed = this.client.util.embed()
+                .setColor(colors.DC)
+                .setDescription(desc)
+                .addField('Mods', mods, true)
+                .addField('Boosters', first.join('\n'), true)
+                .addField('\u200b', second.join('\n'), true);
+
+            return message.util.send(embed);
         }
 
         if (message.channel.type !== 'news' || !message.channel.permissionsFor(this.client.user).has(Permissions.FLAGS.MANAGE_MESSAGES)) return;
@@ -30,5 +40,14 @@ export default class extends Listener {
         if (!channels.includes(message.channel.id)) return;
 
         await message.crosspost().catch(e => this.client.log(e.stack, 'error'));
+    }
+
+    private formatData(str: string): string {
+        return str
+            .split('\r\n\r\n')
+            .join('\n')
+            .replace(/\[(.+?)\]\((https?:\/\/[a-zA-Z0-9/.(]+?)\)/g, '$1')
+            .replace(/##\*\*(Mods|Boosters)\*\*/g, '\n**$1**')
+            .replaceAll('amp:', '');
     }
 }
