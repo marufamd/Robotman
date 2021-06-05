06b130ffd2342d24aca647ce82cb4355bffc35d5
maruf99 <25273018+maruf99@users.noreply.github.com>
2021-06-05 15:14:09 -0400

    fix: rec channel inhibitor

--- src/inhibitors/rec-channel.ts
+++ src/inhibitors/rec-channel.ts
@@ -5,15 +5,12 @@ import { recChannels } from '../util/constants';
 export default class extends Inhibitor {
     public constructor() {
         super('rec-channel', {
-            reason: 'Not a recommendation channel',
-            type: 'all'
+            reason: 'Not a recommendation channel'
         });
     }
 
     public exec(message: Message) {
-        if (message.util?.parsed?.command?.id !== 'recs' ||
-            /^taste test$/i.test(message.content) ||
-            /^writers? rec(ommendation)?s$/i.test(message.content)) return;
+        if (message.util?.parsed?.command?.id !== 'recs') return false;
 
         return !recChannels.includes(message.channel.id);
     }
--- src/listeners/client/message.ts
+++ src/listeners/client/message.ts
@@ -1,7 +1,7 @@
 import { Listener } from 'discord-akairo';
 import { Message, Permissions } from 'discord.js';
 import { scrapeRedditWiki, split } from '../../util';
-import { colors, tasteTestText } from '../../util/constants';
+import { colors, recChannels, tasteTestText } from '../../util/constants';
 
 const clean = (p: string): string => p.replace(/recsbot\/tastetest\/(.+)recs(?:mod)?/g, '$1');
 
@@ -14,56 +14,58 @@ export default class extends Listener {
     }
 
     public async exec(message: Message) {
-        if (/^taste test$/i.test(message.content)) {
-            const { data } = await scrapeRedditWiki(`pages`, 'DCcomics');
-            const list = (data as string[]).filter(p => p.startsWith('recsbot/tastetest/'));
+        if (recChannels.includes(message.channel.id)) {
+            if (/^taste test$/i.test(message.content)) {
+                const { data } = await scrapeRedditWiki(`pages`, 'DCcomics');
+                const list = (data as string[]).filter(p => p.startsWith('recsbot/tastetest/'));
 
-            const mods = list
-                .filter(p => p.endsWith('mod'))
-                .map(clean)
-                .sort();
+                const mods = list
+                    .filter(p => p.endsWith('mod'))
+                    .map(clean)
+                    .sort();
 
-            const boosters = list
-                .filter(p => !p.endsWith('mod'))
-                .map(clean)
-                .sort();
+                const boosters = list
+                    .filter(p => !p.endsWith('mod'))
+                    .map(clean)
+                    .sort();
 
-            const columns = split(boosters, 15);
+                const columns = split(boosters, 15);
 
-            const embed = this.client.util.embed()
-                .setColor(colors.DC)
-                .setDescription(tasteTestText)
-                .addField('Mods', mods.join('\n'), true);
+                const embed = this.client.util.embed()
+                    .setColor(colors.DC)
+                    .setDescription(tasteTestText)
+                    .addField('Mods', mods.join('\n'), true);
 
-            for (const column of columns) {
-                embed.addField(embed.fields.length === 1 ? 'Boosters' : '\u200b', column.join('\n'), true);
+                for (const column of columns) {
+                    embed.addField(embed.fields.length === 1 ? 'Boosters' : '\u200b', column.join('\n'), true);
+                }
+
+                return message.util.send(embed);
             }
 
-            return message.util.send(embed);
-        }
+            if (/^writers? rec(ommendation)?s$/i.test(message.content)) {
+                const { data: { content_md: data } } = await scrapeRedditWiki(`recsbot/writersrecs`, 'DCcomics');
+                const [text, writers] = data.split('\r\n\r\n');
 
-        if (/^writers? rec(ommendation)?s$/i.test(message.content)) {
-            const { data: { content_md: data } } = await scrapeRedditWiki(`recsbot/writersrecs`, 'DCcomics');
-            const [text, writers] = data.split('\r\n\r\n');
+                const list = (writers as string)
+                    .split('\n')
+                    .map(w => w
+                        .replace(/\[(.+?)\]\((https?:\/\/[a-zA-Z0-9/.(]+?)\)/g, '$1')
+                        .trim())
+                    .sort();
 
-            const list = (writers as string)
-                .split('\n')
-                .map(w => w
-                    .replace(/\[(.+?)\]\((https?:\/\/[a-zA-Z0-9/.(]+?)\)/g, '$1')
-                    .trim())
-                .sort();
+                const columns = split(list, 20);
 
-            const columns = split(list, 20);
+                const embed = this.client.util.embed()
+                    .setColor(colors.DC)
+                    .setDescription(text);
 
-            const embed = this.client.util.embed()
-                .setColor(colors.DC)
-                .setDescription(text);
+                for (const column of columns) {
+                    embed.addField('\u200b', column.join('\n'), true);
+                }
 
-            for (const column of columns) {
-                embed.addField('\u200b', column.join('\n'), true);
+                return message.util.send(embed);
             }
-
-            return message.util.send(embed);
         }
 
         if (message.channel.type !== 'news' || !message.channel.permissionsFor(this.client.user).has(Permissions.FLAGS.MANAGE_MESSAGES)) return;
