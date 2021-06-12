522cd1f0bccb5411315c91599e13ff1b04377093
maruf99 <25273018+maruf99@users.noreply.github.com>
2021-06-12 14:37:23 -0400

    fix: tictactoe

--- src/structures/TicTacToe.ts
+++ src/structures/TicTacToe.ts
@@ -89,7 +89,7 @@ export default class TicTacToe {
         }
 
         await move.update({
-            content: ticTacToe.messages.turn(this.player1, this.player2, this.getOtherPlayer(player)),
+            content: ticTacToe.messages.turn(this.player1, this.player2, player),
             components: this.updateBoard(move.customID, player.id === firstPlayer.id)
         });
 
