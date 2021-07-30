964086c06131f747a7e275efe7f4457ea4589a7e
maruf99 <25273018+maruf99@users.noreply.github.com>
2021-07-30 15:53:56 -0400

    chore: remove postgres

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@
 require('dotenv').config();
 
 import 'reflect-metadata';
+import '#util/extensions';
 
 import { assignOptions, Command, Listener } from '#util/commands';
 import { PRODUCTION, ScheduleTime } from '#util/constants';
-import '#util/extensions';
 import { log } from '#util/logger';
 import {
     Client,
@@ -16,7 +16,6 @@ import {
 } from 'discord.js';
 import { RecurrenceRule, scheduleJob } from 'node-schedule';
 import { join } from 'node:path';
-import postgres, { Notice } from 'postgres';
 import readdirp from 'readdirp';
 import { container } from 'tsyringe';
 
@@ -36,22 +35,9 @@ const client = new Client({
     ]
 });
 
-const sql = postgres(process.env.POSTGRES_URL, {
-    onnotice: (notice: Notice) => {
-        if (/relation "(.*)" already exists/gi.test(notice.message)) return;
-        return log(
-            notice,
-            'info',
-            { code: true },
-            { title: 'Postgres' }
-        );
-    }
-});
-
 const commands = new Collection<string, Command>();
 
 container.register(Client, { useValue: client });
-container.register('sql', { useValue: sql });
 container.register('commands', { useValue: commands });
 
 async function init() {
@@ -62,25 +48,6 @@ async function init() {
         .on(Constants.Events.WARN, info => log(info, 'warn'))
         .on(Constants.Events.SHARD_RECONNECTING, () => log('Attempting to reconnect...', 'info'))
         .on(Constants.Events.SHARD_RESUME, () => log('Reconnected'));
-
-    await sql.begin(async sql => {
-        await sql`create table if not exists auto_responses(
-                id          serial primary key,
-                name        text,
-                guild       varchar(25),
-                content     text,
-                attachments text[] default array[]::text[],
-                aliases     text[] default array[]::text[],
-                author	    varchar(25),
-                editor      varchar(25),
-                created_at  timestamp default current_timestamp,
-                updated_at  timestamp,
-                uses        integer default 0,
-                wildcard    boolean,
-                embed       boolean,
-                color       integer default 0
-            );`;
-    });
 
     const commandFiles = readdirp(join(__dirname, 'commands'), { fileFilter: '*.js' });
     const listenerFiles = readdirp(join(__dirname, 'listeners'), { fileFilter: '*.js' });
