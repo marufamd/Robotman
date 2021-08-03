8e5e62d38de8b23ca0980c4c9c6e1cdb5a8a4b44
maruf99 <25273018+maruf99@users.noreply.github.com>
2021-08-03 18:51:45 -0400

    fix: disallow slash commands in dms

--- src/listeners/interaction-create.ts
+++ src/listeners/interaction-create.ts
@@ -11,7 +11,16 @@ export default class implements Listener {
 	public event = Constants.Events.INTERACTION_CREATE;
 
 	public async handle(interaction: CommandInteraction) {
+		if (!interaction.isCommand()) return;
+
 		try {
+			if (interaction.channel.type === 'DM') {
+				return interaction.reply({
+					content: 'Slash commands are not available in Direct Messages.',
+					ephemeral: true
+				});
+			}
+
 			if (!(interaction.channel as TextChannel).permissionsFor(this.client.user.id).has(Permissions.FLAGS.SEND_MESSAGES)) return;
 
 			const command = this.commands.get(interaction.commandName);
