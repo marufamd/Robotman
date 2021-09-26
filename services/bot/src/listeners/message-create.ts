af5ff3e63a979cb5157e03b832da55d3906b8c4d
maruf99 <25273018+maruf99@users.noreply.github.com>
2021-09-25 21:18:37 -0400

    fix(bot): crosspost checking

--- services/bot/src/listeners/message-create.ts
+++ services/bot/src/listeners/message-create.ts
@@ -3,28 +3,21 @@ import type { Listener } from '#util/commands';
 import { handleListenerError, handleMessageCommand, parseCommand } from '#util/commands';
 import { Channels } from '#util/constants';
 import { log } from '#util/logger';
-import type { Message, PermissionResolvable, TextChannel } from 'discord.js';
+import type { Message, TextChannel } from 'discord.js';
 import { Constants, Permissions } from 'discord.js';
 
 export default class implements Listener {
 	public event = Constants.Events.MESSAGE_CREATE;
 
 	public async handle(message: Message) {
-		const hasPermission = (permission: PermissionResolvable) =>
-			(message.channel as TextChannel).permissionsFor(message.client.user).has(permission);
-
 		try {
-			if (
-				message.channel.type === 'GUILD_NEWS' &&
-				hasPermission(Permissions.FLAGS.MANAGE_MESSAGES) &&
-				Object.values(Channels.NEWS).includes(message.channel.id)
-			) {
+			if (message.crosspostable && Object.values(Channels.NEWS).includes(message.channel.id)) {
 				await message.crosspost().catch((e) => log(e.stack, 'error'));
 			}
 
 			if (message.author.bot || message.system || message.webhookId !== null) return;
 
-			if (!hasPermission(Permissions.FLAGS.SEND_MESSAGES)) return;
+			if (!(message.channel as TextChannel).permissionsFor(message.client.user).has(Permissions.FLAGS.SEND_MESSAGES)) return;
 
 			if (await handleLists(message)) return;
 
